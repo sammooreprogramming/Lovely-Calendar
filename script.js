2b3c938f8ec473cbc1c640b0ef2443f570e80ee3
Sam Moore <sammooresemail23@gmail.com>
2022-06-28 14:20:59 -0700

    pre loop details

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     // this shows the current time and the current date
     $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-// Now to set up an event listener for user's input 
-$(".saveBtn").on ("click", function () {
+    // Now to set up an event listener for user's input 
+    $(".saveBtn").on ("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
@@ -10,8 +10,14 @@ $(".saveBtn").on ("click", function () {
     localStorage.setItem(time, text);
 })
 
-
-
-
-
+    // 
+    $("#hour9.description").val(localStorage.getItem(hour9));
+    $("#hour10.description").val(localStorage.getItem(hour10));
+    $("#hour11.description").val(localStorage.getItem(hour11));
+    $("#hour12.description").val(localStorage.getItem(hour12));
+    $("#hour1.description").val(localStorage.getItem(hour1));
+    $("#hour2.description").val(localStorage.getItem(hour2));
+    $("#hour3.description").val(localStorage.getItem(hour3));
+    $("#hour4.description").val(localStorage.getItem(hour4));
+    $("#hour5.description").val(localStorage.getItem(hour5));
 }
