383329d6d5dd42a7dfcf39b5da175c8a06ff7d32
Sam Moore <sammooresemail23@gmail.com>
2022-06-28 16:12:43 -0700

    If else complete

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
     // this shows the current time and the current date
     $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
     // Now to set up an event listener for user's input 
@@ -8,7 +10,7 @@ $(document).ready(function() {
 
     // store the items in the local storage
     localStorage.setItem(time, text);
-})
+    })
 
     // retrieve any saved data from localStorage
     $("#hour9.description").val(localStorage.getItem(hour9));
@@ -22,12 +24,28 @@ $(document).ready(function() {
     $("#hour5.description").val(localStorage.getItem(hour5));
 
     //use moment.js  in a function to get the number of hours that is current for the specified time
-
-    function hourTracker() {
+    function hourTracker() 
         var currentHour = moment().hour();
-    }
+    
     // A loop through the time blocks
-    $(".time-block").each(function ()) {
+    $(".time-block").each(function ()) 
         var blockhour = parseInt($(this).attr("id").split("hour")[1]);
-    }
-}
+    //  if/else/else if statement is used to determine which class is given according to time of day
+    if (blockhour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        }
+    else if (blockhour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        }
+   
+    hourTracker();
+    })
