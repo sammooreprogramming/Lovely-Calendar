c23293a59136fb43fc9c24ff84dc71cde1796917
Sam Moore <sammooresemail23@gmail.com>
2022-06-30 15:30:45 -0700

    total rework

---
+++ super-disco-main/Develop/script.js
@@ -0,0 +1,25 @@
+// VARIABLES //
+
+
+//create new variables for time slots using their respective HTML id
+var hour9 = $('#hour9').text();
+var hour10 = $('#hour10').text();
+var hour11 = $('#hour11').text();
+var hour12 = $('#hour12').text();
+var hour1 = $('#hour1').text();
+var hour2 = $('#hour2').text();
+var hour3 = $('#hour3').text();
+var hour4 = $('#hour4').text();
+var hour5 = $('#hour5').text();
+
+//
+
+
+
+// TIME //
+
+
+
+//display time & date on in jumbotron
+
+// create class and an if statement based on time of day for class assignment
